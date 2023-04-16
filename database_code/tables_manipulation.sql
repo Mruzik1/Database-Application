7b4b7a37ae49553a4a64928e2866984e2c6a55ea
Mruzik1 <mimisha2014@gmail.com>
2023-04-16 21:33:38 +0200

    marked my tasks

--- database_code/tables_manipulation.sql
+++ database_code/tables_manipulation.sql
@@ -2,4 +2,25 @@
 -- Other triggers (responsible for the constraints) are implemented at the end of "create_tables.sql".
 -- P.S. I created only two, but there should be more triggers to handle constraints ragarding different cases. It's just too many of those...
 
+
 -- Non-trivial select #1
+
+-- Non-trivial select #2
+
+
+-- 2x join
+
+-- 3x join
+
+-- Outer join
+
+
+-- Aggregation
+
+-- Grouping 
+
+
+-- Set operators
+
+
+-- Nested select
