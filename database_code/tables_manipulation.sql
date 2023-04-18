133b7668aa4b64d450a76d126c9b1392b2092507
Mruzik1 <mimisha2014@gmail.com>
2023-04-19 00:14:58 +0200

    finished 1 non-trivial

--- database_code/tables_manipulation.sql
+++ database_code/tables_manipulation.sql
@@ -4,15 +4,34 @@
 
 
 -- Non-trivial select #1
--- Here I'm getting a view of server stats (number of users, channels, roles, etc)
+-- Here I'm getting a view of server stats (number of users, channels, roles)
 CREATE VIEW server_stats AS
-    SELECT s.server_id, s.server_name, COUNT(DISTINCT usm.discord_user_id)
+    SELECT 
+        s.server_id, 
+        COUNT(DISTINCT usm.discord_user_id) users, 
+        COUNT(DISTINCT c.channel_id) channels, 
+        COUNT(DISTINCT r.user_role_id) roles
     FROM server s
-    INNER JOIN user_server_map usm ON s.server_id = usm.server_id
-    GROUP BY s.server_id, s.server_name;
-    
+        JOIN user_server_map usm ON s.server_id = usm.server_id
+        JOIN channel c ON s.server_id = c.server_id
+        LEFT JOIN user_role r ON s.server_id = r.server_id
+    GROUP BY s.server_id;
 
 -- Non-trivial select #2
+-- Here is an avarage number of reactions per message on every server
+CREATE VIEW avg_reactions AS
+    SELECT
+        s.server_id,
+        AVG(COUNT(DISTINCT r.reaction_id))
+    FROM server s
+        JOIN reaction r ON s.server_id = 
+            (SELECT c.server_id
+            FROM channel c
+            WHERE r.message_id = 
+                (SELECT m.message_id
+                FROM message m
+                WHERE m.channel_id = c.channel_id))
+    GROUP BY s.server_id;
 
 
 -- 2x join
